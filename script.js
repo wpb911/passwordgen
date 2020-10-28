e7c997c5f7016e33653fcbcb7d509790226d7724
Omega <wpb911@gmail.com>
2020-10-27 21:19:48 -0400

    added all combinations to script.js

--- script.js
+++ script.js
@@ -4,76 +4,119 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  //var password = "testPassword"
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 function generatePassword() {
     // lower case array
     var lowerArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    //upper case array 
+    // upper case array 
     var upperArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-    //number array 
+    // number array 
     var numberArr = ["0","1","2","3","4","5","6","7","8","9"];
-    //special character arrauy 
+    // special character array 
     var specialArr = [" ","!","\"","#","$","%","&","'","(",")","*","+","\,","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
-    // total character array based on choices 
+    
+    // complete character array based on choices 
     var wholeChar = [];
-    //final password array 
+    /*
+    wholeChar = wholeChar.concat(lowerArr) ;
+    wholeChar = wholeChar.concat(upperArr) ;
+    wholeChar = wholeChar.concat(numberArr) ;
+    wholeChar = wholeChar.concat(specialArr) ; 
+     */
+
+    // verify the variables meet the required criteria
+    var verify = false ;
+
+    // final password array after password computation
     var pwdArr = [];
-    //prompt for number of characters in password 
+    
+    // prompt for number of characters in password 
     var numCount = prompt("How many characters do you want in your password (choose between 8 - 128");
-    // logical choices 
+    
+    // logical choices for charater types desired 
     var lowerLet = confirm("Do you desire lower case letters in your password");
     var upperLet = confirm("Do you desire upper case letters in your password");
     var numbers = confirm("Do you desire numbers in your password");
     var specials = confirm("Do you desire special characters in your password");
-    
-    
+
     if ( (parseInt(numCount) >= 8 && parseInt(numCount) <= 128) && (lowerLet || upperLet || numbers || specials) ) {
         alert("Your password will be generated.");
-        if (lowerLet){
-            wholeChar = wholeChar.concat(lowerArr) ;
+        alert("lowlet= " + lowerLet);
+        alert("uplet= " + upperLet);
+        alert("num= " + numbers);
+        alert("spec= " + specials);
+        verify = true ;
 
+        // only lower case letters in password 
+        if (lowerLet && !upperLet && !numbers && !specials ) {
+            wholeChar = wholeChar.concat(lowerArr);
+            alert("Whole Char lower = " + wholeChar);
+            verify = true ;
         }
-        if (upperLet){
-            wholeChar = wholeChar.concat(upperArr) ;
-
+        // only upper case letters in password
+        else if (!lowerLet && upperLet && !numbers && !specials ) {
+            wholeChar = wholeChar.concat(upperArr);
+            alert("Whole Char upper = " + wholeChar);
+            verify = true ;
         }
-        
-        if (numbers){
-            wholeChar = wholeChar.concat(numberArr) ;
-
+        // only numbers in password  
+        else if (!lowerLet && !upperLet && numbers && !specials ) {
+            wholeChar = wholeChar.concat(numberArr);
+            alert("Whole Char number = " + wholeChar);
+            verify = true ;
         }
-        if (specials){
-            wholeChar = wholeChar.concat(specialArr) ;
-
+        // only special characters in password 
+        else if (!lowerLet && !upperLet && !numbers && specials ) {
+            wholeChar = wholeChar.concat(specialArr);
+            alert("Whole Char special = " + wholeChar);
         }
-
-        for(var i = 0; i < numCount; i++){
-            var num = Math.floor(Math.random() * 10)
-            pwdArr.push(wholeChar[num]);
-            console.log("numCount = " + numCount);
+        // only lowercase and uppercase characters in password 
+        else if (lowerLet && upperLet && !numbers && !specials) {
+            wholeChar = wholeChar.concat(lowerArr);
+            wholeChar = wholeChar.concat(upperArr);
+            alert("Whole Char both upper and lower= " + wholeChar);
             
-            console.log(" num = " + num);
-            console.log(" pwd = " + pwdArr[i]);
-
-
-
+        }
+        // only lowercase and numeric characters in password 
+        else if (lowerLet && !upperLet && numbers && !specials) {
+            wholeChar = wholeChar.concat(lowerArr);
+            wholeChar = wholeChar.concat(numberArr);
+            alert("Whole Char both lower and numeric = " + wholeChar);
+            
+        }
+        // only lowercase and special characters in password 
+        else if (lowerLet && !upperLet && !numbers && specials) {
+            wholeChar = wholeChar.concat(lowerArr); 
+            wholeChar = wholeChar.concat(specialArr); 
 
         }
-        return (pwdArr.join(''));
-        
-    }   
-    else {
-        alert("You must pick at least one character type and password length between 8 - 128 to generate a password.");
     }
+    
 
+        if (verify) {
+            for(var i = 0; i < numCount; i++){
+                var num = Math.floor(Math.random() * wholeChar.length)
+                pwdArr.push(wholeChar[num]);
+                console.log("numCount = " + numCount);
+                console.log(" whole = " + wholeChar[num]);
+                console.log(" num = " + num);
+                console.log(" pwd = " + pwdArr[i]);
+            }
+        }
+        else {
+            alert("You must pick at least one valid character type and password length between 8 - 128 to generate a password.");
+            verify = false;
+        }
+        
+        return (pwdArr.join(''));
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
